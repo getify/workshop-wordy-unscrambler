b3aa5a711378b6e8983a9215ad7702a77fa1ec1b
Kyle Simpson <getify@gmail.com>
2023-03-13 21:54:16 -0400

    implementing (alternate) option 4 algorithms

--- js/wordy.js
+++ js/wordy.js
@@ -6,17 +6,55 @@ export default {
 
 // ****************************
 
+// DAWG (directed acyclic word graph)
+var dict = new MinimalWordGraph();
+
 function loadWords(wordList) {
-	// TODO: implement a data structure for the array
-	// `wordList` parameter; return the number
-	// of entries inserted into the data structure
-	return 0;
+	if (dict.size() > 0) {
+		dict = new MinimalWordGraph();
+	}
+
+	for (let word of wordList) {
+		dict.add(word);
+	}
+
+	// performance optimization
+	dict.makeImmutable();
+
+	return dict.size();
+}
+
+function countLetters(str) {
+	var counts = {};
+	for (let letter of str) {
+		counts[letter] = (counts[letter] || 0) + 1;
+	}
+	return counts;
 }
 
 function findWords(input) {
-	// TODO: implement unscrambling/searching logic
-	// for a string of uppercase letters in the
-	// `input` parameter; return the array of
-	// matching words
-	return [];
+	// collect counts of input letters
+	var inputCounts = countLetters(input);
+
+	// de-duplicate the input characters
+	input = [ ...(new Set(input)) ];
+
+	// search the minimum-word-graph for words matching the
+	// input letters
+	var words = dict.containsOnly(
+		Array.isArray(input) ? input :
+		typeof input == "string" ? input.split("") :
+		[]
+	);
+
+	// filter out words whose letter counts exceed the input letter counts
+	return words.filter(function removeWords(word){
+		var wordLetterCounts = countLetters(word);
+		for (let [ letter, count ] of Object.entries(wordLetterCounts)) {
+			if (!inputCounts[letter] || count > inputCounts[letter]) {
+				return false;
+			}
+		}
+		return true;
+	});
 }
